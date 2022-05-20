8ad175e63aa87f0a6cb769dfe5d285b06a4763a3
yosho_cho <choyosho165152987@gmail.com>
2022-05-21 00:35:14 +0900

    InputProviderを専用の名前空間に入れた

--- Assets/Scripts/Utility/InputProvider.cs
+++ Assets/Scripts/Utility/InputProvider.cs
@@ -1,30 +1,33 @@
 using UnityEngine;
 
-public class InputProvider : IInputProvider
+namespace InputProviders
 {
-    PlayerInput _inputAction;
+    public class InputProvider : IInputProvider
+    {
+        PlayerInput _inputAction;
 
-    public void SetUp(PlayerInput input)
-    {
-        _inputAction = input;
-    }
-    public bool GetAttack()
-    {
-        return _inputAction.Player.Attack.WasPressedThisFrame();
-    }
+        public void SetUp(PlayerInput input)
+        {
+            _inputAction = input;
+        }
+        public bool GetAttack()
+        {
+            return _inputAction.Player.Attack.WasPressedThisFrame();
+        }
 
-    public bool GetAvoid()
-    {
-        return _inputAction.Player.Avoid.WasPressedThisFrame();
-    }
+        public bool GetAvoid()
+        {
+            return _inputAction.Player.Avoid.WasPressedThisFrame();
+        }
 
-    public bool GetJump()
-    {
-        return _inputAction.Player.Jump.WasPressedThisFrame();
-    }
+        public bool GetJump()
+        {
+            return _inputAction.Player.Jump.WasPressedThisFrame();
+        }
 
-    public Vector3 GetMoveDirection()
-    {
-        throw new System.NotImplementedException();
+        public Vector3 GetMoveDirection()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
