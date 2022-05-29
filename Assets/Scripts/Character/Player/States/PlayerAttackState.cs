3de6a9e66d65163a032ff2a86ed55ecaffd68a6d
yosho_cho <choyosho165152987@gmail.com>
2022-05-29 17:29:57 +0900

    プレイヤーの攻撃処理を作成

--- Assets/Scripts/Character/Player/States/PlayerAttackState.cs
+++ Assets/Scripts/Character/Player/States/PlayerAttackState.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using AttackSetting;
 using UnityEngine;
 using State = StateMachine<NewPlayer>.State;
 
@@ -9,15 +8,30 @@ public partial class NewPlayer : CharacterBase
     {
         protected override void OnEnter(State prevState)
         {
-            base.OnEnter(prevState);
+            owner._currentVelocity = Vector3.zero;
+            owner._actionCtrl.RequestAction(AttackType.Weak);
         }
         protected override void OnUpdate()
         {
-            base.OnUpdate();
+            if (owner._inputProvider.GetAttack() && !owner._actionCtrl.ActionKeep)
+            {
+                if (owner.IsGround()) owner._actionCtrl.RequestAction(AttackType.Weak);
+                else owner._actionCtrl.RequestAction(AttackType.Airial);
+            }
+
+            if ((owner.IsGround()))
+            {
+                if (owner._inputAxis.sqrMagnitude > 0.1f && 
+                    owner._actionCtrl.ReceiveTimer < owner._actionCtrl.CurrentAction.ReceiveTime - 0.1f)
+                    owner.ChangeState(StateEvent.Walk);
+                else if (!owner._animCtrl.IsPlayingAnimatin()) owner.ChangeState(StateEvent.Idle);
+            }
+            else if (!owner._actionCtrl.ActionKeep) owner.ChangeState(StateEvent.Fall);
+
         }
         protected override void OnExit(State nextState)
         {
-            base.OnExit(nextState);
+            owner._actionCtrl.EndAttack();
         }
     }
 }
