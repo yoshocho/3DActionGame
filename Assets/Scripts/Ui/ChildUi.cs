653093a6418a051cfde03442a2674193a20885cf
yosho_cho <choyosho165152987@gmail.com>
2022-06-06 20:53:22 +0900

    Uiを管理するスクリプトを作成途中

---
+++ Assets/Scripts/Ui/ChildUi.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ChildUi : MonoBehaviour
+{
+
+    public abstract void SetUp();
+
+    public virtual void Enable() { }
+
+    public virtual void Disable() { }
+}
---
+++ Assets/Scripts/Ui/UiPanel.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiPanel : MonoBehaviour
+{
+    [SerializeField]
+    bool _initVisibility = true;
+    protected CanvasGroup _canvasGroup;
+    protected CanvasRenderer _canvasRenderer;
+    public RectTransform RectTrans{ get; protected set; }
+
+    List<ChildUi> _childUis = new List<ChildUi>();
+
+    public List<ChildUi> ChildUis => _childUis;
+
+    private void Awake()
+    {
+        RectTrans = GetComponent<RectTransform>();
+        _canvasGroup = GetComponent<CanvasGroup>();
+        _canvasRenderer = GetComponent<CanvasRenderer>();
+        if (!_initVisibility) _canvasGroup.alpha = 0.0f;
+        SetUp();
+    }
+
+    protected virtual void SetUp()
+    {
+        ChildUi[] childUis = GetComponentsInChildren<ChildUi>();
+        foreach (ChildUi childUi in childUis) 
+        {
+            childUi.SetUp();
+            _childUis.Add(childUi);
+        }
+    }
+
+    public virtual void Open()
+    {
+        foreach(ChildUi childUi in _childUis)
+        {
+            childUi.Enable();
+        }
+        _canvasGroup.alpha = 1.0f;
+    }
+    public virtual void Close()
+    {
+        foreach (ChildUi childUi in _childUis)
+        {
+            childUi.Disable();
+        }
+        _canvasGroup.alpha = 0.0f;
+    }
+}
