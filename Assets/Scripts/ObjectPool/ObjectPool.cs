7acbc00fa45bff8aacfd9684892eadb814d3a623
yosho_cho <choyosho165152987@gmail.com>
2022-06-22 16:45:18 +0900

    オブジェクトプールクラスをネームスペースに入れた、オブジェクト取得関数の名前修正しコメント追加

--- Assets/Scripts/ObjectPool/ObjectPool.cs
+++ Assets/Scripts/ObjectPool/ObjectPool.cs
@@ -2,51 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPool<T> where T : MonoBehaviour, IPoolObject
+namespace ObjectPool
 {
-    private T _origin;
-    private Transform _poolParent;
-    private List<T> _objcts = new List<T>();
-
-
-    public void SetUp(T poolTarget,int count = 10,Transform parent = null)
+    /// <summary>
+    /// オブジェクトプールクラス
+    /// </summary>
+    /// <typeparam name="T">プールにするオブジェクトの型</typeparam>
+    public class ObjectPool<T> where T : MonoBehaviour, IPoolObject
     {
-        _origin = poolTarget;
-        _objcts = new List<T>();
-        if (parent == null) _poolParent = new GameObject(poolTarget.name + " Pool").transform;
-        else _poolParent = parent;
+        private T _origin;
+        private Transform _poolParent;
+        private List<T> _objcts = new List<T>();
 
-        for (int i = 0; i < count; i++)
+        /// <summary>
+        /// プールのセットアップ
+        /// </summary>
+        /// <param name="poolTarget">プールオブジェクト</param>
+        /// <param name="count">最初の生成数</param>
+        /// <param name="parent">プールをまとめるtransform</param>
+        public void SetUp(T poolTarget, int count = 10, Transform parent = null)
         {
+            _origin = poolTarget;
+            _objcts = new List<T>();
+            if (parent == null) _poolParent = new GameObject(poolTarget.name + " Pool").transform;
+            else _poolParent = parent;
+
+            for (int i = 0; i < count; i++)
+            {
+                var newObj = Object.Instantiate(_origin, _poolParent);
+                newObj.gameObject.SetActive(false);
+                newObj.SetUp();
+                _objcts.Add(newObj);
+            }
+        }
+        /// <summary>
+        /// 使われてないオブジェクトを取り出す
+        /// </summary>
+        /// <returns>使われてないオブジェクト</returns>
+        public T Get()
+        {
+            foreach (var obj in _objcts)
+            {
+                if (obj.gameObject.activeSelf) continue;
+                obj.gameObject.SetActive(true);
+                obj.SetUp();
+                return obj;
+            }
+
             var newObj = Object.Instantiate(_origin, _poolParent);
-            newObj.gameObject.SetActive(false);
             newObj.SetUp();
             _objcts.Add(newObj);
+            return newObj;
         }
-    }
-
-    public T Get()
-    {
-        foreach (var obj in _objcts) 
+        /// <summary>
+        /// プールを削除
+        /// </summary>
+        public void Clear()
         {
-            if (obj.gameObject.activeSelf) continue;
-            obj.gameObject.SetActive(true);
-            obj.SetUp();
-            return obj;
-        }
-
-        var newObj = Object.Instantiate(_origin, _poolParent);
-        newObj.SetUp();
-        _objcts.Add(newObj);
-        return newObj;
-    }
-
-    public void Clear()
-    {
-        foreach(var obj in _objcts) 
-        {
-            Object.Destroy(obj.gameObject);
-            _objcts.Clear();
+            foreach (var obj in _objcts)
+            {
+                Object.Destroy(obj.gameObject);
+                _objcts.Clear();
+            }
         }
     }
 }
