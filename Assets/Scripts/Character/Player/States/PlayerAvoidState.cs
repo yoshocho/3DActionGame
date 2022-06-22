1a63837550b4401621a8b921be1cfcba83fa0e49
yosho_cho <choyosho165152987@gmail.com>
2022-06-22 20:26:16 +0900

    新しく作ったプレイヤー回避ステートに回避処理を追加

--- Assets/Scripts/Character/Player/States/PlayerAvoidState.cs
+++ Assets/Scripts/Character/Player/States/PlayerAvoidState.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using State = StateMachine<NewPlayer>.State;
 
@@ -7,17 +5,52 @@ public partial class NewPlayer : CharacterBase
 {
     public class PlayerAvoidState : State
     {
+        Vector3 _avoidAxis = Vector3.zero;
+        float _avoidTimer = 0.0f;
+        float _justTimer = 0.0f;
         protected override void OnEnter(State prevState)
         {
-            
+            if(owner._debagMode)Debug.Log("InAvoid");
+
+            _avoidAxis = owner._moveForward.normalized;
+            owner.PlayAnimation("Avoid");
+            owner._inAvoid = true;
+            _avoidTimer = owner._avoidTime;
+            _justTimer = owner._justTime;
         }
         protected override void OnUpdate()
         {
-            
+            _avoidTimer -= Time.deltaTime;
+            _justTimer -= Time.deltaTime;
+            if (_justTimer < 0.0f)
+            {
+                owner._inAvoid = false;
+                _justTimer = 0.0f;
+            }
+
+            if (_avoidTimer > 0.0f)
+            {
+                if (owner._inputAxis.sqrMagnitude < 0.05) _avoidAxis = owner._selfTrans.forward * owner._avoidSpeed;
+                _avoidAxis.y = 0.0f;
+                owner._targetRot = Quaternion.LookRotation(_avoidAxis);
+                owner._currentVelocity = _avoidAxis * owner._avoidSpeed;
+            }
+            else
+            {
+                if (owner.IsGround())
+                {
+                    if (owner._inputAxis.sqrMagnitude < 0.1f) owner.ChangeState(StateEvent.Walk);
+                    else owner.ChangeState(StateEvent.Idle);
+                    if (owner._inputProvider.GetJump()) owner.ChangeState(StateEvent.Jump);
+                }
+                else owner.ChangeState(StateEvent.Fall);
+            }
         }
         protected override void OnExit(State nextState)
         {
-            
+            owner._inAvoid = false;
+            _avoidTimer = 0.0f;
+            _justTimer = 0.0f;
         }
     }
 }
