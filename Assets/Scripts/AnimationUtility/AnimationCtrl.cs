1126a1452c53a6fa69b2d29a91c5fcbc8979467a
yosho_cho <choyosho165152987@gmail.com>
2022-06-25 23:59:00 +0900

    命名規則修正

--- Assets/Scripts/AnimationUtility/AnimationCtrl.cs
+++ Assets/Scripts/AnimationUtility/AnimationCtrl.cs
@@ -13,6 +13,8 @@ public class AnimClip
     public float Duration = 0.1f;
     [Range(-0.5f, 2.0f)]
     public float Speed = 1.0f;
+    [SerializeField]
+    public bool UseRootMotion = true;
 }
 
 [System.Serializable]
@@ -25,60 +27,63 @@ public class AnimState
 public class AnimationCtrl : MonoBehaviour
 {
     public delegate void CallBack(int param);
-    CallBack m_eventCallBack;
+    CallBack _eventCallBack;
 
     [SerializeField]
-    Animator m_anim;
+    Animator _anim;
 
-    AnimatorOverrideController m_animatorOverrideController;
+    AnimatorOverrideController _animatorOverrideController;
 
     private void Awake()
     {
-        if (!m_anim) m_anim = GetComponentInChildren<Animator>();
+        if (!_anim) _anim = GetComponentInChildren<Animator>();
 
-        m_animatorOverrideController = new AnimatorOverrideController();
-        m_animatorOverrideController.runtimeAnimatorController = m_anim.runtimeAnimatorController;
-        m_anim.runtimeAnimatorController = m_animatorOverrideController;
+        _animatorOverrideController = new AnimatorOverrideController(_anim.runtimeAnimatorController);
+        _anim.runtimeAnimatorController = _animatorOverrideController;
     }
 
     public void Active()
     {
-        m_anim.enabled = true;
+        _anim.enabled = true;
     }
 
     public void DisActive()
     {
-        m_anim.enabled = false;
+        _anim.enabled = false;
     }
 
     public void ForceUpdate()
     {
-        m_anim.Update(0.0f);
+        _anim.Update(0.0f);
     }
 
     public void SetNormalizedTime(float time, int layer = 0)
     {
-        m_anim.Play(0, layer, time);
-        m_anim.Update(0.0f);
+        _anim.Play(0, layer, time);
+        _anim.Update(0.0f);
     }
 
-    //public void Play(string stateName,float dur = 0.1f,int layer = 0)
-    //{
-    //Active();
-    //m_anim.CrossFadeInFixedTime(stateName, dur, layer);
-    //}
+    
     public void Play(string stateName, float dur = 0.1f, int layer = 0, Action onAnimEnd = null)
     {
         Active();
-        m_anim.CrossFadeInFixedTime(stateName, dur, layer);
+        _anim.CrossFadeInFixedTime(stateName, dur, layer);
         StartCoroutine(AnimEndCallBack(0, onAnimEnd));
     }
-
     //public void PlayAndCallBack(string stateName, float dur = 0.1f, int layer = 0, Action onAnimEnd = null)
     //{
 
     //}
 
+    public void SetRootAnim() 
+    {
+        _anim.applyRootMotion = true;
+    }
+    public void SetNormalAnim()
+    {
+        _anim.applyRootMotion = false;
+    }
+    
     public IEnumerator AnimEndCallBack(int layer = 0, Action onAnimEnd = null)
     {
         yield return null;
@@ -95,34 +100,34 @@ public class AnimationCtrl : MonoBehaviour
     public void ChangeClip(string stateName, AnimationClip clip, int layerId = 0)
     {
 
-        AnimatorStateInfo[] layerInfo = new AnimatorStateInfo[m_anim.layerCount];
-        for (int i = 0; i < m_anim.layerCount; i++)
+        AnimatorStateInfo[] layerInfo = new AnimatorStateInfo[_anim.layerCount];
+        for (int i = 0; i < _anim.layerCount; i++)
         {
-            layerInfo[i] = m_anim.GetCurrentAnimatorStateInfo(i);
+            layerInfo[i] = _anim.GetCurrentAnimatorStateInfo(i);
         }
-        m_animatorOverrideController[stateName] = clip;
-        m_anim.Update(0.0f);
-        for (int i = 0; i < m_anim.layerCount; i++)
+        _animatorOverrideController[stateName] = clip;
+        _anim.Update(0.0f);
+        for (int i = 0; i < _anim.layerCount; i++)
         {
-            m_anim.Play(layerInfo[i].nameHash, i, layerInfo[i].normalizedTime);
+            _anim.Play(layerInfo[i].nameHash, i, layerInfo[i].normalizedTime);
         }
     }
 
     public bool IsPlayingAnimatin(int layer = 0)
     {
-        var state = m_anim.GetCurrentAnimatorStateInfo(layer);
+        var state = _anim.GetCurrentAnimatorStateInfo(layer);
         if (state.loop) return true;
         return state.normalizedTime < 1.0f;
     }
 
     public void SetEventDelegate(CallBack cb)
     {
-        m_eventCallBack = cb;
+        _eventCallBack = cb;
     }
 
     public void AnimationEvent(int evtType)
     {
-        m_eventCallBack?.Invoke(evtType);
+        _eventCallBack?.Invoke(evtType);
     }
 
 }
