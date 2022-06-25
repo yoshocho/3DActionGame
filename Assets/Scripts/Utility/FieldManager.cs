093922b81d88ea1d43641c2360d17bd0e1feff5f
yosho_cho <choyosho165152987@gmail.com>
2022-06-25 19:53:26 +0900

    フィールドマネージャーに敵のスポーンに関する処理を追加

--- Assets/Scripts/Utility/FieldManager.cs
+++ Assets/Scripts/Utility/FieldManager.cs
@@ -9,26 +9,60 @@ public class FieldManager : MonoBehaviour
 
     [SerializeField]
     GameObject _enemyPrefab;
-    
+
+    [SerializeField]
+    GameData _gameData;
+    public GameData GameData => _gameData;
+
+    int _waveCount = 0;
+    List<Transform> _spawnPoints = new List<Transform>();
+    List<CharacterBase> _fieldEnemy = new List<CharacterBase>();
+    [SerializeField]
+    int _fieldCount = 0;
+    WaveData _currentWave;
     ObjectPool<NormalStateEnemy> _enemys = new ObjectPool<NormalStateEnemy>();
 
     private void Awake()
     {
         Instance = this;
-
-        _enemys.SetUp(_enemyPrefab.GetComponent<NormalStateEnemy>(), transform);
+        _enemys.SetUp(_enemyPrefab.GetComponent<NormalStateEnemy>(), transform,10);
     }
     private void Start()
     {
-        
+        var points = GameObject.FindGameObjectsWithTag("SpawnPoint");
+        for (int i = 0; i < points.Length; i++)
+        {
+            _spawnPoints.Add(points[i].transform);
+        }
+        //_currentWave = GameData.WaveDatas[0];
+        _fieldCount = GameData.WaveDatas[0].WaveCount;
     }
 
     private void Update()
     {
+        if (!GameManager.Instance.GameStart) return;
         
+        if(_fieldCount == GameData.WaveDatas[_waveCount].WaveCount)
+        {
+            for (int i = 0; i < _spawnPoints.Count; i++)
+            {
+                Debug.Log("Sp");
+                var enemy = _enemys.Get();
+                Spawn(enemy.gameObject, _spawnPoints[i].transform.position);
+            }
+            _fieldCount = 0;
+            _waveCount++;
+        }
+        //if (Input.GetKeyDown(KeyCode.J))
+        //{
+        //    _fieldCount++;
+        //}
     }
+    public void SpawnWave() 
+    {
 
-    public void SpawnEnemy(GameObject enemyObj,Vector3 point) 
+    }
+    void Spawn(GameObject enemyObj,Vector3 point) 
     {
         enemyObj.transform.position = point;
     }
