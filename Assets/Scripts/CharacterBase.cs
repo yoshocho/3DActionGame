aa2a05912819164ca1ff7fd4c0ae8077ff0a9894
yosho_cho <choyosho165152987@gmail.com>
2022-07-03 20:53:51 +0900

    チャラクターのステータスクラスや基底クラスを修正、キャラクターの基底クラスにあったパラメーターをステータスに移し、名前を修正

--- Assets/Scripts/CharacterBase.cs
+++ Assets/Scripts/CharacterBase.cs
@@ -7,42 +7,34 @@ using AttackSetting;
 public class CharacterBase : MonoBehaviour,IDamage
 {
     [SerializeField]
-    StatusModel _status = new StatusModel();
+    public CharaStatusModel Status { get; protected set; } = new CharaStatusModel();
 
     [SerializeField]
     float _moveSpeed = default;
-    public IReadOnlyReactiveProperty<int> CurrentHp { get => _status.hp; protected set { _status.hp.Value = value.Value; } }
-    public int MaxHp { get => _status.maxHp; private set { _status.maxHp = value; }}
     public float MoveSpeed { get => _moveSpeed;protected set { _moveSpeed = value; } }
-    public int Atk { get => _status.atk; protected set { _status.atk = value;} }
-
     public bool IsDeath { get; protected set; } = false;
 
     Rigidbody _rb;
-    public Rigidbody Rigidbody { get => _rb; protected set { _rb = value; } }
+    public Rigidbody RB { get => _rb; protected set { _rb = value; } }
     
     private void Awake()
     {
-        OnAwake();
+        SetUp();
     }
 
-    protected virtual void OnAwake()
+    protected virtual void SetUp()
     {
         _rb = GetComponent<Rigidbody>();
-        _status.maxHp = _status.hp.Value;
+        Status.SetUp();
     }
 
     public virtual void AddDamage(int damage, AttackType attackType = AttackType.Weak)
     {
-        _status.hp.Value = Mathf.Max(_status.hp.Value - damage,0);
-        if(_status.hp.Value == 0)
+        var hp = Mathf.Max(Status.CurrentHp.Value - damage, 0);
+        Status.UpdateHp(hp);
+        if(Status.CurrentHp.Value == 0)
         {
             IsDeath = true;
         }
-    }
-
-    public virtual void KnockBack()
-    {
-
     }
 }
