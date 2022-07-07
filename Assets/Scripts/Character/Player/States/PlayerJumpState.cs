91df2f9c9822b041c631e6d112a78c1351240717
yosho_cho <choyosho165152987@gmail.com>
2022-07-07 23:54:17 +0900

    ジャンプボタンを連打しても、少し時間を開けてからジャンプするようにした

--- Assets/Scripts/Character/Player/States/PlayerJumpState.cs
+++ Assets/Scripts/Character/Player/States/PlayerJumpState.cs
@@ -7,6 +7,8 @@ partial class NewPlayer : CharacterBase
 {
     public class PlayerJumpState : State
     {
+        bool _canJump = true;
+        const float _waitTime = 0.3f;
         protected override void OnEnter(State prevState)
         {
             if(owner._debagMode) Debug.Log("Jump");
@@ -14,19 +16,27 @@ partial class NewPlayer : CharacterBase
             owner.PlayAnimation("Jump",0.1f);
             if (owner._inputAxis.magnitude > 0.1f) owner._targetRot = Quaternion.LookRotation(owner._moveForward);
             owner._currentVelocity = new Vector3(owner._moveForward.x,owner._jumpPower,owner._moveForward.z);
-
+            owner.StartCoroutine(WaitJump());
         }
         protected override void OnUpdate()
         {
             if (owner._inputAxis.sqrMagnitude > 0.1f) owner._targetRot = Quaternion.LookRotation(owner._moveForward);
             if (owner._inputProvider.GetAvoid()) owner.ChangeState(StateEvent.Avoid);
             if (owner._inputProvider.GetAttack()) owner.ChangeState(StateEvent.Attack);
-            if (owner._inputProvider.GetJump()) owner.ChangeState(StateEvent.Jump);
+
+            if (owner._inputProvider.GetJump() && _canJump) owner.ChangeState(StateEvent.Jump);
+            
             if (owner._currentVelocity.y < 0.0f) owner.ChangeState(StateEvent.Fall);
         }
         protected override void OnExit(State nextState)
         {
 
+        }
+        IEnumerator WaitJump()
+        {
+            _canJump = false;
+            yield return new WaitForSeconds(_waitTime);
+            _canJump = true;
         }
     }
 }
