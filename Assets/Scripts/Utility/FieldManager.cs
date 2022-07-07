364eb30451cbf5998bcf2f863b98e8bd09d74504
yosho_cho <choyosho165152987@gmail.com>
2022-07-07 23:52:14 +0900

    フィールドマネージャーを修正

--- Assets/Scripts/Utility/FieldManager.cs
+++ Assets/Scripts/Utility/FieldManager.cs
@@ -6,64 +6,75 @@ using ObjectPool;
 public class FieldManager : MonoBehaviour
 {
     public FieldManager Instance { get; private set; }
-
-    [SerializeField]
-    GameObject _enemyPrefab;
-
     [SerializeField]
     GameData _gameData;
     public GameData GameData => _gameData;
 
-    int _waveCount = 0;
-    List<Transform> _spawnPoints = new List<Transform>();
-    List<CharacterBase> _fieldEnemy = new List<CharacterBase>();
     [SerializeField]
+    public int ClearCount = 20;
+   
+    [SerializeField]
+    GameObject _enemyPrefab;
+
+    List<Transform> _spawnPoints = new List<Transform>();
+
+    ObjectPool<NormalStateEnemy> _enemyPool = new ObjectPool<NormalStateEnemy>();
+
+    float _timer;
     int _fieldCount = 0;
-    WaveData _currentWave;
-    ObjectPool<NormalStateEnemy> _enemys = new ObjectPool<NormalStateEnemy>();
+    int _currentWaveCount = 0;
+    int _spawnCount = 0;
 
     private void Awake()
     {
         Instance = this;
-        _enemys.SetUp(_enemyPrefab.GetComponent<NormalStateEnemy>(), transform,10);
     }
+
     private void Start()
     {
+        _enemyPool.SetUp(_enemyPrefab.GetComponent<NormalStateEnemy>(), transform, 10);
         var points = GameObject.FindGameObjectsWithTag("SpawnPoint");
         for (int i = 0; i < points.Length; i++)
         {
             _spawnPoints.Add(points[i].transform);
         }
-        //_currentWave = GameData.WaveDatas[0];
-        _fieldCount = GameData.WaveDatas[0].WaveCount;
+
+        _fieldCount = GameData.WavesData[0].EnemyCount;
     }
+
+    //void Enemy
 
     private void Update()
     {
         if (!GameManager.Instance.GameStart) return;
-        
-        if(_fieldCount == GameData.WaveDatas[_waveCount].WaveCount)
+
+        UpdateEnemy();
+    }
+
+    public void UpdateEnemy()
+    {
+        if (_spawnCount >= _spawnPoints.Count) return;
+
+        if (_fieldCount >= GameData.WavesData[_currentWaveCount].EnemyCount)
         {
             for (int i = 0; i < _spawnPoints.Count; i++)
             {
-                Debug.Log("Sp");
-                var enemy = _enemys.Get();
+                var enemy = _enemyPool.Get();
                 Spawn(enemy.gameObject, _spawnPoints[i].transform.position);
+                if (_spawnCount == GameData.WavesData[_currentWaveCount].EnemyCount) break;
             }
-            _fieldCount = 0;
-            _waveCount++;
-        }
-        //if (Input.GetKeyDown(KeyCode.J))
-        //{
-        //    _fieldCount++;
-        //}
-    }
-    public void SpawnWave() 
-    {
+            _currentWaveCount++;
+            _fieldCount = GameData.WavesData[_currentWaveCount].EnemyCount;
+            if (_currentWaveCount == GameData.WavesData.Count)
+            {
+                Debug.Log("I—¹");
+            }
 
+        }
     }
-    void Spawn(GameObject enemyObj,Vector3 point) 
+    void Spawn(GameObject enemyObj, Vector3 point)
     {
         enemyObj.transform.position = point;
+        _spawnCount++;
     }
 }
