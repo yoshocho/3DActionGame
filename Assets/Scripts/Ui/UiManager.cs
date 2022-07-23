894dab62ae4455770461df3db9f16be2dfaff369
yosho_cho <choyosho165152987@gmail.com>
2022-07-23 17:19:09 +0900

    UIマネージャーを修正　探すCanvas名を変更し、nullチェック処理を追加

--- Assets/Scripts/Ui/UiManager.cs
+++ Assets/Scripts/Ui/UiManager.cs
@@ -4,19 +4,37 @@ using UnityEngine;
 using System.Linq;
 using UnityEngine.UI;
 
-public class UiManager : Singleton<UiManager>
+public class UiManager
 {
+
+    private static UiManager s_instance = new UiManager();
+    private UiManager()
+    {
+
+    }
+    public static UiManager Instance => s_instance;
+    
+
     List<UiPanel> _uiList;
 
     UiPanel _currentUi;
     int _prevUiIndex;
 
+    const string _canvasName = "MasterCanvas";
+    public Canvas MasterCanvas { get; private set; }
+
     public void SetUp()
     {
-
+        MasterCanvas = GameObject.Find(_canvasName).GetComponent<Canvas>(); 
+        UiPanel[] panels = MasterCanvas.GetComponentsInChildren<UiPanel>();
 
         _uiList = new List<UiPanel>();
 
+        foreach (UiPanel panel in panels)
+        {
+            panel.SetUp();
+            _uiList.Add(panel);
+        }
     }
 
     public void ReceiveData<T>(T arg)
@@ -24,17 +42,25 @@ public class UiManager : Singleton<UiManager>
         foreach (var parent in _uiList)
         {
             var ui = parent.ChildUis.OfType<IUIEventReceiver<T>>();
-            ui.FirstOrDefault().ReceiveData(arg);
-            break;
+            if (ui != null)
+            {
+                ui.FirstOrDefault().ReceiveData(arg);
+                break;
+            }
         }
     }
 
     public void ReceiveData<T>(string parentName, T arg)
     {
-        var parent = _uiList
-            .FirstOrDefault(p => p.name == parentName)
+        var ui = _uiList.FirstOrDefault(p => p.Path == parentName)
             .ChildUis.OfType<IUIEventReceiver<T>>().FirstOrDefault();
-        parent.ReceiveData(arg);
+
+        if(ui == null)
+        {
+            Debug.LogWarning(string.Format("Žw’è‚³‚ê‚½UI‚ÍŒ©‚Â‚©‚è‚Ü‚¹‚ñ‚Å‚µ‚½{0},{1}",parentName,arg));
+            return;
+        }
+        ui.ReceiveData(arg);
     }
 
     public void RequestOpen(string path)
