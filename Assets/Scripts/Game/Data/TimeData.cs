a1e709e9ff46b4725201fefe52444d842696e3b1
yosho_cho <choyosho165152987@gmail.com>
2022-07-25 14:54:00 +0900

    時間を管理するクラスを作成した

---
+++ Assets/Scripts/Game/Data/TimeData.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeData
+{
+    public float ElapsedTime { get; private set; }
+    public int Minite { get; private set; }
+    public int Hour { get; private set; }
+
+    public void UpdateTime()
+    {
+        ElapsedTime += Time.deltaTime;
+
+        if(ElapsedTime > 60)
+        {
+            Minite++;
+            ElapsedTime -= 60.0f;
+        }
+        if(Minite >= 60)
+        {
+            Hour++;
+            Minite = 0;
+        }
+    }
+    public void ResetTime()
+    {
+        ElapsedTime = 0.0f;
+        Minite = 0;
+        Hour = 0;
+    }
+}
