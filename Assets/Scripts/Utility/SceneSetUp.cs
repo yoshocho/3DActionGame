007d12badeaedd1899b202fa56340376c159ccb3
yosho_cho <choyosho165152987@gmail.com>
2022-08-26 18:52:02 +0900

    シーンセットアップクラスを修正、フェード後に実行されるUnityEventをActionに変更

--- Assets/Scripts/Utility/SceneSetUp.cs
+++ Assets/Scripts/Utility/SceneSetUp.cs
@@ -1,19 +1,31 @@
 using UnityEngine;
-using UnityEngine.Events;
+using System;
 
 public class SceneSetUp : MonoBehaviour
 {
     [SerializeField]
     GameManager.GameState _gameState;
-    [SerializeField]
-    UnityEvent _onSceneStart;
-
+    
+    Action _onSceneStart;
+    public static SceneSetUp Instance { get; private set; } = null;
+    
     private void Awake()
     {
+        Instance = this;
+        GameManager.Instance.SetUp(_gameState);
         GameManager.Instance.GameStateEvent(_gameState);
+    }
+
+    public void SetInit(Action setUp)
+    {
+        _onSceneStart += setUp;
     }
     private void Start()
     {
         FadeSystem.FadeIn(() => _onSceneStart?.Invoke());
+    }
+    private void OnDestroy()
+    {
+        Instance = null;
     }
 }
