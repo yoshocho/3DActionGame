83e14a1ee6add2c9ca1bc7fcda633e8e0e453867
yosho_cho <choyosho165152987@gmail.com>
2022-08-28 19:00:50 +0900

    エフェクトマネージャーにこれから使うエフェクト用の関数を追加し、テスト中

--- Assets/Scripts/Utility/EffectManager.cs
+++ Assets/Scripts/Utility/EffectManager.cs
@@ -1,30 +1,90 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+/// <summary>
+/// 攻撃エフェクトなどの管理クラス
+/// </summary>
 public class EffectManager : MonoBehaviour
 {
     [System.Serializable]
-    public class Effect 
+    public class Effect
     {
         public string Name = "";
         public GameObject EfObject = null;
     }
 
     public static EffectManager Instance { get; private set; } = null;
-    Dictionary<string, GameObject> m_effectDic = new Dictionary<string, GameObject>();
+    Dictionary<string, GameObject> _effectDic = new Dictionary<string, GameObject>();
 
-    [SerializeField] List<Effect> m_effects = new List<Effect>();
+    [SerializeField]
+    List<Effect> _effects = new List<Effect>();
+    [SerializeField]
+    DamageUi _damageUi;
+
+    float _stopTime = default;
+    float _frameTimer = default;
+    float _timeScale = default;
+    bool _isHitStop = default;
 
     private void Awake()
     {
         Instance = this;
-        m_effects.ForEach(ef => m_effectDic.Add(ef.Name,ef.EfObject));
+        _timeScale = Time.timeScale;
+        _effects.ForEach(ef => _effectDic.Add(ef.Name,ef.EfObject));
     }
 
-    static public GameObject PlayEffect(string key, Vector3 pos,Quaternion rot = default)
+    private void Update()
     {
-        if (!Instance.m_effectDic.ContainsKey(key)) return null;
-        return Instantiate(Instance.m_effectDic[key],pos,rot);
+        if (_isHitStop && Time.timeScale == _timeScale)
+        {
+            _frameTimer = 0;
+            _timeScale = Time.timeScale;
+            Time.timeScale = 0;
+            _isHitStop = false;
+        }
+        if (Time.timeScale == 0 && _frameTimer < _stopTime)
+        {
+            _frameTimer += Time.unscaledDeltaTime;
+            if (_frameTimer >= _stopTime)
+            {
+                Time.timeScale = _timeScale;
+            }
+        }
+    }
+
+    public void HitStop(float power)
+    {
+        _stopTime = power * 1.0f / 24.0f;
+        _isHitStop = true;
+    }
+
+    public static GameObject PlayEffect(string key, Vector3 pos,Quaternion rot = default)
+    {
+        if (!Instance._effectDic.ContainsKey(key)) return null;
+        return Instantiate(Instance._effectDic[key],pos,rot);
+    }
+
+    public static GameObject PlayEffect(GameObject effectObj,Vector3 pos,Quaternion rot = default)
+    {
+        return Instantiate(effectObj,pos,rot);
+    }
+
+    public static void DamageUiPop(GameObject target, int damage,Color color)
+    {
+        var go = Instantiate(Instance._damageUi.gameObject, Instance.transform.parent);
+        var dmg = go.GetComponent<DamageUi>();
+        dmg.Set(target,damage,Color.black);
+    }
+
+    public IEnumerator ControllerShake(Vector2 shakeVec, float shakeTime)
+    {
+        if (Gamepad.current == null) yield break;
+
+        var gamePad = Gamepad.current;
+        gamePad.SetMotorSpeeds(shakeVec.x, shakeVec.y);
+        yield return new WaitForSeconds(shakeTime);
+        gamePad.SetMotorSpeeds(default,default);
     }
 }
