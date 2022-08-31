57f1ca01f0fba77bf0830957b1231681ee11999d
yosho_cho <choyosho165152987@gmail.com>
2022-08-31 18:36:04 +0900

    敵のスポーン処理を作成途中

--- Assets/Scripts/Utility/NewFieldManager.cs
+++ Assets/Scripts/Utility/NewFieldManager.cs
@@ -1,34 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class NewFieldManager : MonoBehaviour
 {
     [SerializeField]
-    List<EnemyGroupTest> _enemys = new List<EnemyGroupTest>();
+    List<EnemyGroupTest> _waveData = new List<EnemyGroupTest>();
 
     public FieldData FieldData { get; private set; } = new FieldData();
 
-    List<Transform> _spawnPoints = new List<Transform>();
+    public int CurrentWave { get; private set; } = 0;
+    int _spawanCount = 0;
+    [SerializeField]
+    float _spawnTime = 0.5f;
+    float _spawnTimer;
 
     [SerializeField]
-    float _spawnTime;
-    float _spawnTimer;
+    Vector3 _spawnCenter = Vector3.zero;
+    [SerializeField]
+    Vector3 _spawnLength = new Vector3(30.0f, 0.5f, 30.0f);
 
     private void Start()
     {
-        var points = GameObject.FindGameObjectsWithTag("SpawnPoint");
-
-        foreach (var point in points)
-        {
-            _spawnPoints.Add(point.transform);
-        }
+        
 
     }
 
     private void Update()
     {
 
+
+
+        _spawnTimer += Time.deltaTime;
+
+        if (_spawnTimer > _spawnTime)
+        {
+            
+            var enemy = _waveData[CurrentWave].Enemys[_spawanCount];
+            Instantiate(enemy, GetRandomPos(), Quaternion.identity);
+            _spawanCount++;
+
+            if (_spawanCount >= _waveData[CurrentWave].Enemys.Count)
+            {
+                CurrentWave++;
+                _spawanCount = 0;
+            }
+
+        }
+
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    var testObj =  GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //    Instantiate(testObj, GetRandomPos(), Quaternion.identity);
+        //}
+    }
+
+    Vector3 GetRandomPos()
+    {
+        float x = Random.Range(-_spawnLength.x,_spawnLength.x) / 2;
+        float z = Random.Range(-_spawnLength.z,_spawnLength.z) / 2;
+
+
+        return new Vector3(x,0.5f,z) + _spawnCenter;
+    }
+    [Conditional("UNITY_EDITOR")]
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(_spawnCenter,_spawnLength);
     }
 }
 [System.Serializable]
