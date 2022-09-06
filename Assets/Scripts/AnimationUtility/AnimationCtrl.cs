034b5ea3a6458e8ca500dceb0018d7cd419166d7
yosho_cho <choyosho165152987@gmail.com>
2022-09-06 23:10:57 +0900

    アニメーション再生管理クラスを修正

--- Assets/Scripts/AnimationUtility/AnimationCtrl.cs
+++ Assets/Scripts/AnimationUtility/AnimationCtrl.cs
@@ -77,17 +77,13 @@ public class AnimationCtrl : MonoBehaviour
     }
 
 
-    public void Play(string stateName, float dur = 0.1f, int layer = 0, Action onAnimEnd = null)
+    public void Play(string stateName, float dur = 0.1f, int layerId = 0, Action onAnimEnd = null)
     {
         Active();
-        _anim.CrossFadeInFixedTime(stateName, dur, layer);
-        StartCoroutine(AnimEndCallBack(0, onAnimEnd));
+        _anim.CrossFadeInFixedTime(stateName, dur, layerId);
+
+        if(onAnimEnd != null)StartCoroutine(AnimEndCallBack(layerId, onAnimEnd));
     }
-    //public void PlayAndCallBack(string stateName, float dur = 0.1f, int layer = 0, Action onAnimEnd = null)
-    //{
-
-    //}
-
     public void SetRootAnim()
     {
         _anim.applyRootMotion = true;
@@ -97,10 +93,10 @@ public class AnimationCtrl : MonoBehaviour
         _anim.applyRootMotion = false;
     }
 
-    public IEnumerator AnimEndCallBack(int layer = 0, Action onAnimEnd = null)
+    public IEnumerator AnimEndCallBack(int layerId = 0, Action onAnimEnd = null)
     {
         yield return null;
-        yield return new WaitUntil(() => !IsPlayingAnimatin(layer));
+        yield return new WaitUntil(() => !IsPlayingAnimatin(layerId));
         onAnimEnd?.Invoke();
     }
     /// <summary>
