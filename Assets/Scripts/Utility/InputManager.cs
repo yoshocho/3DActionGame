e54c2f23ff1a936881f513a388755f986285604e
yosho_cho <choyosho165152987@gmail.com>
2022-09-11 22:48:58 +0900

    不必要な関数を削除

--- Assets/Scripts/Utility/InputManager.cs
+++ Assets/Scripts/Utility/InputManager.cs
@@ -4,11 +4,7 @@ using InputProviders;
 public class InputManager
 {
     private static InputManager s_instance = new InputManager();
-    private InputManager() 
-    {
-        //SetUp();
-       
-    }
+    private InputManager() { }
     public static InputManager Instance => s_instance;
 
     PlayerInput _inputActions;
@@ -22,15 +18,6 @@ public class InputManager
         ServiceLocator<IInputProvider>.Register(inputProvider);
         Debug.Log("InputProviderをRegist");
     }
-
-    public void SetProvider()
-    {
-        //var player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStateMachine>();
-        var inputProvider = new InputProvider();
-        inputProvider.SetUp(_inputActions);
-        //player.ReceiveInputProvider(inputProvider);
-    }
-
     public void Disable() => _inputActions?.Disable();
 
     public void Dispose() => _inputActions?.Dispose();
