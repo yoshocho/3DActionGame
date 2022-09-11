022a7fb0419e6e15d24fd8ec0edb7bbc804b27b9
yosho_cho <choyosho165152987@gmail.com>
2022-09-11 22:28:24 +0900

    カーソルを管理するクラスをstaticクラスから普通のクラスにした

--- Assets/Scripts/Utility/CursorManager.cs
+++ Assets/Scripts/Utility/CursorManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class CursorManager
+public class CursorManager
 {
     public static void CursorCtrl(bool visible, CursorLockMode lockMode)
     {
